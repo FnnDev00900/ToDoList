2e78ba3165647882eeb76816f9340d0103d4e291
Fnn00 <fnn00900@gmail.com>
2025-09-23 11:56:20 +0330

    Add Viewmodel for task and add event handler for ui tasts

---
+++ app/src/main/java/com/fnndev/todolist/ui/tasks/TasksViewModel.kt
@@ -0,0 +1,36 @@
+package com.fnndev.todolist.ui.tasks
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fnndev.todolist.data.repository.TaskRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class TasksViewModel @Inject constructor(private val repository: TaskRepository) : ViewModel() {
+
+    val tasks = repository.getAllTasks()
+
+    fun onEvent(event: TasksScreenEvents) {
+        when (event) {
+
+            TasksScreenEvents.OnAddTaskClick -> TODO()
+
+            is TasksScreenEvents.OnDeleteTask -> {
+                viewModelScope.launch(Dispatchers.IO) {
+                    repository.deleteTask(event.task)
+                }
+            }
+
+            is TasksScreenEvents.OnIsDoneChange -> {
+                viewModelScope.launch(Dispatchers.IO) {
+                    repository.upsertTask(event.task.copy(isCompleted = !event.task.isCompleted))
+                }
+            }
+
+            is TasksScreenEvents.OnTaskClick -> TODO()
+        }
+    }
+}
